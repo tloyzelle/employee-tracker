6781402b5016bdf3411402f6097541e5f1e77772
Taegan Loyzelle <tloyzelle@gmail.com>
2021-07-22 19:46:12 -0400

    adding functions behind adding

--- emptrak.js
+++ emptrak.js
@@ -37,3 +37,167 @@ function startPrompt() {
         }
     ])
 }
+
+//view employees
+function viewAllEmployees() {
+    connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, " " ,e.last_name) AS Manager FROM employee INNER JOIN role on role_id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_if = e.id',
+    function (err, res) {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+}
+
+//view roles
+function viewAllRoles() {
+    connection.query('SELECT employee.first_name, employee.last_name, role.title AS TITLE FROM employee JOIN role ON employee.role_id = role.id;',
+    function(err, res) {
+        if (err) throw errconsole.table(res)
+        startPrompt()
+    })
+}
+
+//view departments
+function viewAllDepartments() {
+    connection.query('SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee FROM employee JOIN role ON employee.role_id = role.id JOIN  department ON role.department_id = department.id ORDER BY employee.id;',
+    function(err, res) {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+}
+
+
+//adding employees
+function addEmployee() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees role id?',
+            name: 'addEmployRole'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees manger id?',
+            name: 'addManager'
+        }
+    ])
+    .then(function (res) {
+        const firstName = res.firstName;
+        const lastName = res.lastName;
+        const employRoleID = res.addEmployRole;
+        const employManID = res.addManager;
+        const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${employRoleID}" "${emplyManID}")`;
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            console.table(res);
+            startPrompt()
+        })
+    })
+}
+
+//updating employee
+function updateEmployee() {
+    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+     if (err) throw err
+     console.log(res)
+     inquirer
+     .prompt([
+       {
+         type: "input",
+         message: "Enter the employee's ID you want to be updated",
+         name: "updateEmploy"
+       },
+       {
+         type: "input",
+         message: "Enter the new role ID for that employee",
+         name: "newRole"
+       }
+     ])
+     .then(function (res) {
+         const updateEmploy = res.updateEmploy;
+         const newRole = res.newRole;
+         const queryUpdate = `UPDATE employee SET role_id = "${newRole}" WHERE id = "${updateEmploy}"`;
+         connection.query(queryUpdate, function (err, res) {
+           if (err) {
+             throw err;
+           }
+           console.table(res);
+            startPrompt()
+        })
+  
+    });
+  });
+
+  }
+
+//adding employee role
+function addRole() { 
+    connection.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+      inquirer.prompt([
+          {
+            name: "Title",
+            type: "input",
+            message: "What is the roles Title?"
+          },
+          {
+            name: "Salary",
+            type: "input",
+            message: "What is the Salary?"
+  
+          } 
+      ]).then(function(res) {
+          connection.query(
+              "INSERT INTO role SET ?",
+              {
+                title: res.Title,
+                salary: res.Salary,
+              },
+              function(err) {
+                  if (err) throw err
+                  console.table(res);
+                  startPrompt();
+              }
+          )
+  
+      });
+    });
+    }
+
+//adding department
+function addDepartment() { 
+
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add?"
+        }
+    ]).then(function(res) {
+        var query = connection.query(
+            "INSERT INTO department SET ? ",
+            {
+              name: res.name
+            
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                startPrompt();
+            }
+        )
+    })
+  }
