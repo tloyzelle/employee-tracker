438a74439162990553fa505a844d05cb2405b24c
Taegan Loyzelle <tloyzelle@gmail.com>
2021-07-22 20:44:31 -0400

    debugging mysql workbemnch

--- schema.sql
+++ schema.sql
@@ -11,23 +11,23 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL,
-    salary DECIMAL(10,2) NULL,
-    department_id INT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id),
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    role_id INT NULL,
-    manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES manager(id),
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
+  PRIMARY KEY (id)
 );
 
 INSERT INTO department (name)
